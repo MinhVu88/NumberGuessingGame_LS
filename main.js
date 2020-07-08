f03c2677e2949a5138349b8adb0dfced4141e569
Minh Vu <minhvu72826@gmail.com>
2020-07-09 05:47:55 +0700

    1st commit

--- main.js
+++ main.js
@@ -1,87 +1,86 @@
 let game = {
-    'min': 1,
-    'max': 10
+  min: 1,
+  max: 10
 };
 
 function showMessage(msg, color) {
-    game.msg.innerHTML = msg;
+  game.msg.innerHTML = msg;
 
-    game.msg.style.color = color || 'black';
-};
+  game.msg.style.color = color || "black";
+}
 
 function getRandomNo(max, min) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 function endGame() {
-    game.btn.innerHTML = 'Reset';
+  game.btn.innerHTML = "Reset";
 
-    game.input.style.display = 'none';
+  game.input.style.display = "none";
 
-    game.btn.classList.add('replay'); // when game's over, guess btn ---> replay btn
+  game.btn.classList.add("replay"); // when game's over, guess btn ---> replay btn
 
-    game.max += 5; // after each reset, max increases by 5
-};
+  game.max += 5; // after each reset, max increases by 5
+}
 
 function guess() {
-    // if user hits replay, init() is called -> replay btn ---> guess btn -> input field shows up
-    if (game.btn.classList.contains('replay')) {
-        init();
+  // if user hits replay, init() is called -> replay btn ---> guess btn -> input field shows up
+  if (game.btn.classList.contains("replay")) {
+    init();
 
-        game.btn.innerHTML = 'Guess';
+    game.btn.innerHTML = "Guess";
 
-        game.input.style.display = 'inline-block';
+    game.input.style.display = "inline-block";
 
-        game.btn.classList.remove('replay');
+    game.btn.classList.remove("replay");
+  } else {
+    // if btn has no replay class, that means user's still guessing
+    game.rounds++;
+
+    let user = parseInt(game.input.value);
+
+    game.input.value = ""; // the input field is blank after each guess entered
+
+    if (user === game.random) {
+      showMessage(`Correct! user: ${user} (in ${game.rounds} rounds) | computer: ${game.random}`);
+
+      game.input.style.borderColor = "green";
+
+      endGame(); // when user wins, game's over
+    } else if (user < game.min || user > game.max) {
+      showMessage("Invalid guess", "crimson");
     } else {
-        // if btn has no replay class, that means user's still guessing
-        game.rounds++;
+      let feedback = user > game.random ? { color: "yellow", content: "user > computer" } : { color: "blue", content: "user < computer" };
 
-        let user = parseInt(game.input.value);
+      showMessage(feedback.content, feedback.color);
 
-        game.input.value = ''; // the input field is blank after each guess entered
-
-        if (user === game.random) {
-            showMessage(`Correct! user: ${user} (in ${game.rounds} rounds) | computer: ${game.random}`);
-
-            game.input.style.borderColor = 'green';
-
-            endGame(); // when user wins, game's over
-        } else if(user < game.min || user > game.max) {
-            showMessage('Invalid guess', 'crimson');
-        } else {
-            let feedback = user > game.random ? {'color': 'yellow', 'content': 'user > computer'} 
-                                              : {'color': 'blue', 'content': 'user < computer'};
-
-            showMessage(feedback.content, feedback.color);
-
-            game.input.style.borderColor = feedback.color;
-        }
-
-        console.log(game.random);
+      game.input.style.borderColor = feedback.color;
     }
-};
+
+    console.log(game.random);
+  }
+}
 
 // initialize the game
 function init() {
-    game.rounds = 0;
+  game.rounds = 0;
 
-    game.random = getRandomNo(game.max, game.min);
+  game.random = getRandomNo(game.max, game.min);
 
-    showMessage(`Let\'s make a wild guess to get a correct number between ${game.min} & ${game.max}`, 'pink');
-};
+  showMessage(`Let\'s make a wild guess to get a correct number between ${game.min} & ${game.max}`, "pink");
+}
 
-// once the DOM's content loads, the main gameplay kicks in 
-document.addEventListener('DOMContentLoaded', () => {
-    game.input = document.querySelector('input');
+// once the DOM's content loads, the main gameplay kicks in
+document.addEventListener("DOMContentLoaded", () => {
+  game.input = document.querySelector("input");
 
-    game.output = document.querySelector('.output');
+  game.output = document.querySelector(".output");
 
-    game.msg = document.querySelector('.message');
+  game.msg = document.querySelector(".message");
 
-    game.btn = document.querySelector('button');
+  game.btn = document.querySelector("button");
 
-    game.btn.addEventListener('click', guess);
+  game.btn.addEventListener("click", guess);
 
-    init();
+  init();
 });
